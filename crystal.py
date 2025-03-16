cb8d5d728ea1588a83edd344e1b4c79a920f1855
EliaOndacs <amirreza.ondacs90@gmail.com>
2025-03-16 06:00:23 +0330

    a shit ton of changes that are not just one subject for me to describe it all here

--- crystal.py
+++ crystal.py
@@ -1,7 +1,12 @@
-from typing import Callable
+from typing import Callable, Literal
 from dataclasses import dataclass, field
 
 _ki: int = -1
+
+
+class StateError(Exception): ...
+
+
 def keygen():
     global _ki
     _ki += 1
@@ -13,21 +18,64 @@ class Component[ReturnType]:
     name: str
     key: str
     state: dict
-    func: Callable[["Component"], ReturnType]
+    func: Callable[..., ReturnType]
 
     props: dict = field(default_factory=dict)
+    _is_mounted: bool = False
     _slot_n: int = field(default_factory=int)
+    _previous_state: dict = field(default_factory=dict)
 
     def setState(self, name: str, value: object):
+        self._previous_state[name] = self.state.get(name)
         self.state[name] = value
+        self.componentDidStateUpdate(name, value)
 
     def getState(self, name: str) -> object:
-        return self.state[name]
+        try:
+            return self.state[name]
+        except KeyError:
+            raise StateError(
+                f"the state {name!r} not found for the component {self.name!r}"
+            )
+
+    def getPrevious(self, name: str) -> object:
+        try:
+            return self._previous_state[name]
+        except KeyError:
+            raise StateError(
+                f"the state {name!r} has just initialised or do not exists for the component {self.name!r}"
+            )
+
+    def componentDidMount(self):
+        """Called after the component is mounted."""
+        pass
+
+    def componentDidUpdate(self):
+        """Called after the component is updated."""
+        pass
+
+    def componentWillUnmount(self):
+        """Called before the component is unmounted."""
+        pass
+
+    def componentDidStateUpdate(self, name: str, value: object):
+        """Called when a state in a component is being updated."""
 
     def render(self) -> ReturnType:
         self._slot_n = 0
+        if self._is_mounted == False:
+            self.componentDidMount()  # Call when the component is mounted
+            self._is_mounted = True
         result = self.func(self, **self.props)
+        if self._is_mounted == True:
+            self.componentDidUpdate()  # Call after rendering
         return result
+
+    def export(self, obj: object):
+        self.__dict__[obj.__name__] = obj  # type: ignore
+
+    def exportas(self, obj: object, name: str):
+        self.__dict__[name] = obj
 
     def useState[Type](self, initial: Type) -> tuple[Type, Callable[[Type], Type]]:
 
@@ -39,15 +87,47 @@ class Component[ReturnType]:
 
         def setMethod(value: Type) -> Type:
             nonlocal name, self
+            self._previous_state[name] = self.state.get(name)
             self.state[name] = value
+            self.componentDidStateUpdate(name, value)
             return self.state[name]
 
         return self.state[name], setMethod
 
+    def useStateName(self):
+        return f"slot_{self._slot_n-1}"
 
-def component[T](func: Callable[[Component[T]], T]) -> Component[T]:
+    def bindLifecycleEvent(
+        self,
+        event: Literal[
+            "componentMount", "componentUnmount", "componentUpdate", "stateUpdate"
+        ],
+        callback: Callable,
+    ):
+        """
+        Binds a callback function to a specific lifecycle event of the component.
+
+        Args:
+            event (Literal): The lifecycle event to bind the callback to.
+                Can be one of the following:
+                - "componentMount": Called when the component is mounted.
+                - "componentUnmount": Called when the component is about to unmount.
+                - "componentUpdate": Called after the component is updated.
+                - "stateUpdate": Called when a state variable is updated.
+            callback (Callable): The function to be called when the specified event occurs.
+        """
+        match event:
+            case "stateUpdate":
+                setattr(self, "componentDidStateUpdate", callback)
+            case "componentMount":
+                setattr(self, "componentDidMount", callback)
+            case "componentUnmount":
+                setattr(self, "componentWillUnmount", callback)
+            case "componentUpdate":
+                setattr(self, "componentDidUpdate", callback)
+
+
+def createComponent[T](func: Callable[..., T]) -> Component[T]:
     c: Component[T] = Component(func.__name__, keygen(), {}, func)
     c.__doc__ = func.__doc__
     return c
-
-
