3272346e34804b36e06462c44d8c4899cf772d02
EliaOndacs <amirreza.ondacs90@gmail.com>
2025-08-26 20:01:12 +0330

    added the new frmaework "fade.py"

---
+++ fade.py
@@ -0,0 +1,27 @@
+"""
+a corelib framework that combines other packages
+from corelib to give you a full client side framework
+to build cli/tui apps or combine with sanic for web development
+"""
+
+from .pipex import *
+from .phelix import *
+from .neon import *
+from .makefs import *
+from sqllex import types, SQLite3x  # type: ignore
+from pydantic import BaseModel
+from .eventquery import *
+from .datatype import *
+from .util import *
+from .workerlib import Worker
+from .rod import r
+from .detacls import deta
+from . import stream
+from .serverdom import *
+from .userbase import *
+from .storage import storage
+import click
+from pathlib import Path
+from .maskslib import *
+
+VERSION = "0.1.0"
