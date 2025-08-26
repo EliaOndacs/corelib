bca6131c454a26ba2aed60226e30da5add1284f8
EliaOndacs <amirreza.ondacs90@gmail.com>
2025-08-26 19:52:04 +0330

    util.py: removed TerminalDriver in favour of neon driver

--- util.py
+++ util.py
@@ -28,52 +28,6 @@ class Driver(Protocol):
     def data(self, obj: object): ...
     def __call__(self, call: object, *args, **kwds): ...
 
-
-class TerminalDriver(Driver):
-    def setup(self):
-        print("started the terminal driver | version: 1.0.0", file=sys.stderr)
-        self.ansi_stack: list[str] = []
-
-    def pushAnsi(self, code: str):
-        self.ansi_stack.append(code)
-        self.data(code)
-
-    def popAnsi(self):
-        if len(self.ansi_stack) <= 1:
-            self.data("\x1b[0m")
-            return ""
-        code = self.ansi_stack.pop()
-        self.data(self.ansi_stack[-1])
-        return code
-
-    def call(self, func: Callable, *args, **kwargs):
-        return func(*args, **kwargs)
-
-    def clear(self):
-        print("\x1b[H\x1b[2J\r", end="", flush=True)
-        return
-
-    def data(self, obj: object):
-        print(obj, end="", flush=True)
-        return
-
-    def _break(self):
-        print("\n", end="", flush=True)
-        return
-
-    def __call__(self, call: object, *args, **kwargs):
-        if callable(call):
-            self.call(call, *args, **kwargs)
-        elif call == "clear":
-            self.clear()
-        elif call == "break":
-            self._break()
-        elif isinstance(call, str):
-            self.data(call)
-        else:
-            raise RuntimeError(f"unsupported driver call: {call!r}")
-
-
 def sequencer[
     T
 ](pattern: str, function: Callable[[str], T], *, wait: float | int = 0) -> Generator[
